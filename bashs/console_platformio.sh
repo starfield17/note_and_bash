5253ff4888648e8b9fbb7be2fc1d355705cbb2f8
starfield <149873042+starfield17@users.noreply.github.com>
2025-05-15 12:19:56 +0800

    Add files via upload

---
+++ bashs/console_platformio.sh
@@ -0,0 +1,4 @@
+curl -fsSL https://raw.githubusercontent.com/platformio/platformio-core/develop/platformio/assets/system/99-platformio-udev.rules | sudo tee /etc/udev/rules.d/99-platformio-udev.rules
+sudo udevadm control --reload-rules
+sudo udevadm trigger
+
